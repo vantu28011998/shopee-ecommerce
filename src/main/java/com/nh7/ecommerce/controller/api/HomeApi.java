dbcb4f0f4fa7139d82199b0ebe9b45fafbf1a14b
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-04-14 16:30:30 +0700

    a

--- src/main/java/com/nh7/ecommerce/controller/api/HomeApi.java
+++ src/main/java/com/nh7/ecommerce/controller/api/HomeApi.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/home")
 @ControllerAdvice
-@CrossOrigin(origins = "http://192.168.31.52:3000")
+@CrossOrigin(origins = {"http://192.168.31.52:3000","http://localhost:3000"})
 public class HomeApi {
     @Autowired
     private CategoryService categoryService;
