ff0a4f02ba295757ba744f86af3d0b09cbdf2962
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-04-17 19:20:36 +0700

    a

---
+++ src/main/java/com/nh7/ecommerce/config/DataSourceConfig.java
@@ -0,0 +1,40 @@
+package com.nh7.ecommerce.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        //Config connection
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        //Config connection pool
+        hikariConfig.setMaximumPoolSize(5);
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setPoolName("springHikariCP");
+        //
+        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
--- src/main/java/com/nh7/ecommerce/model/ProductCardModel.java
+++ src/main/java/com/nh7/ecommerce/model/ProductCardModel.java
@@ -3,6 +3,8 @@ package com.nh7.ecommerce.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+
 @Getter
 @Setter
 public class ProductCardModel {
