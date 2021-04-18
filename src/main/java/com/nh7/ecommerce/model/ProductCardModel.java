dfef903f85d7b2482b25238ace5dac493ca32e24
lamngochuy <lnhuy1912@gmail.com>
2021-04-18 09:18:52 +0700

    a

--- src/main/java/com/nh7/ecommerce/model/ProductCardModel.java
+++ src/main/java/com/nh7/ecommerce/model/ProductCardModel.java
@@ -1,11 +1,18 @@
 package com.nh7.ecommerce.model;
 
+import com.nh7.ecommerce.repository.ProductRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class ProductCardModel {
+    @Autowired
+    private ProductRepository productRepository;
+
     //product
     private Long id;
     private String productThumbnail;
@@ -16,4 +23,13 @@ public class ProductCardModel {
     private String address;
     //order
     private int soldQuantity;
+
+    public ProductCardModel(Long id, String productThumbnail, Double productPrice, String postTitle, String address, int soldQuantity) {
+        this.id = id;
+        this.productThumbnail = productThumbnail;
+        this.productPrice = productPrice;
+        this.postTitle = postTitle;
+        this.address = address;
+        this.soldQuantity = soldQuantity;
+    }
 }
