d0a24b18f2a7933e10b0b55eb474272a89e6db54
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-04-21 23:48:20 +0700

    make it better

--- src/main/java/com/nh7/ecommerce/config/DataSourceConfig.java
+++ src/main/java/com/nh7/ecommerce/config/DataSourceConfig.java
@@ -11,36 +11,36 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class DataSourceConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.datasource.driver-class-name}")
-    private String driver;
-
-    @Bean
-    public DataSource dataSource(){
-        HikariConfig hikariConfig = new HikariConfig();
-        //Config connection
-        hikariConfig.setDriverClassName(driver);
-        hikariConfig.setJdbcUrl(url);
-        hikariConfig.setUsername(username);
-        hikariConfig.setPassword(password);
-        //Config connection pool
-
-        hikariConfig.setMaximumPoolSize(10);
-        hikariConfig.setConnectionTestQuery("SELECT 1");
-        hikariConfig.setPoolName("springHikariCP");
-        //
-        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
-        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
-
-        return new HikariDataSource(hikariConfig);
-    }
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driver;
+//
+//    @Bean
+//    public DataSource dataSource(){
+//        HikariConfig hikariConfig = new HikariConfig();
+//        //Config connection
+//        hikariConfig.setDriverClassName(driver);
+//        hikariConfig.setJdbcUrl(url);
+//        hikariConfig.setUsername(username);
+//        hikariConfig.setPassword(password);
+//        //Config connection pool
+//
+//        hikariConfig.setMaximumPoolSize(10);
+//        hikariConfig.setConnectionTestQuery("SELECT 1");
+//        hikariConfig.setPoolName("springHikariCP");
+//        //
+//        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+//        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+//
+//        return new HikariDataSource(hikariConfig);
+//    }
 }
