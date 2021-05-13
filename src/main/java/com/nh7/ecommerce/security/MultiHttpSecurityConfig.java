e39599b87d6ab69c79bc4edc950627cbe8e7d834
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-05-13 16:15:15 +0700

    OK

--- src/main/java/com/nh7/ecommerce/security/MultiHttpSecurityConfig.java
+++ src/main/java/com/nh7/ecommerce/security/MultiHttpSecurityConfig.java
@@ -61,7 +61,7 @@ public class MultiHttpSecurityConfig {
             // We don't need CSRF for this example
             httpSecurity.csrf().disable()
                     // dont authenticate this particular request
-                    .authorizeRequests().antMatchers("","/login/**","/api/login", "/api/register","/api/home/**").permitAll().
+                    .authorizeRequests().antMatchers("/login/**","/api/login", "/api/register","/api/home/**").permitAll().
                     // all other requests need to be authenticated
                             anyRequest().authenticated().and().
                     // make sure we use stateless session; session won't be used to
