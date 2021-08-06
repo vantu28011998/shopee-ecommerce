0f12896a6df55bbb0cfff0701c1b4cf57003330f
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-08-07 00:49:48 +0700

    fix smtp

--- src/main/java/com/nh7/ecommerce/controller/view/GoogleAuthorized.java
+++ src/main/java/com/nh7/ecommerce/controller/view/GoogleAuthorized.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GoogleAuthorized {
-    @GetMapping("/google2267c49c2ec726ab")
+    @GetMapping("/")
     public String authorizeGoogle(Model model) {
+        return "google2267c49c2ec726ab"; // Trả về file index.html
+    }
+    @GetMapping("/google2267c49c2ec726ab.html")
+    public String authorizeGoogle2(Model model) {
         return "google2267c49c2ec726ab"; // Trả về file index.html
     }
 }
