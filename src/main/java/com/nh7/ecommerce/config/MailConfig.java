31dda4edc52ec4ace50ad978f2e7eae92d05d894
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-08-06 22:49:01 +0700

    fix smtp

---
+++ src/main/java/com/nh7/ecommerce/config/MailConfig.java
@@ -0,0 +1,32 @@
+package com.nh7.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    private String usernameGmail="shope.ecommerce.nh7@gmail.com";
+    private String passwordGmail="shope.ecommerce.28011998";
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(usernameGmail);
+        mailSender.setPassword(passwordGmail);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/nh7/ecommerce/service/MailService.java
+++ src/main/java/com/nh7/ecommerce/service/MailService.java
@@ -3,13 +3,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
     @Autowired
-    private JavaMailSender javaMailSender;
+    public JavaMailSender javaMailSender;
     public void sendEmail(String emailAddress, Integer generatedNum) throws MailException {
         String title="SHOPEE ECOMMERCE NH7";
         String content="MÃ XÁC MINH ĐĂNG KÝ TÀI KHOẢN CỦA BẠN LÀ " + generatedNum +".Có hiệu lực trong 1 phút. Vui lòng KHÔNG chia sẻ mã này với người khác, kể cả nhân viên shopee.";
