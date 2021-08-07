8375f993c3bdc7389e8255ccf9ba01aeda3d62de
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-08-07 17:05:28 +0700

    fix smtp

--- src/main/java/com/nh7/ecommerce/config/MailConfig.java
+++ src/main/java/com/nh7/ecommerce/config/MailConfig.java
@@ -2,6 +2,7 @@ package com.nh7.ecommerce.config;
 
 import java.util.Properties;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,25 +12,75 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 @Configurable
 public class MailConfig {
-    private String usernameGmail="shope.ecommerce.nh7@gmail.com";
-    private String passwordGmail="shope.ecommerce.28011998";
+//    private String usernameGmail="shope.ecommerce.nh7@gmail.com";
+//    private String passwordGmail="shope.ecommerce.28011998";
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername(usernameGmail);
+//        mailSender.setPassword(passwordGmail);
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.smtp.host", "mail.authsmtp.com");
+//        props.put("mail.smtp.port", "mail.authsmtp.com");
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
+
+    @Value("${email.sender.host}")
+    private String host;
+
+    @Value("${email.sender.port}")
+    private String port;
+
+    @Value("${email.sender.username}")
+    private String username;
+
+    @Value("${email.sender.password}")
+    private String password;
+
+    @Value("${email.transport.protocol}")
+    private String protocol;
+
+    @Value("${email.smtp.auth}")
+    private String auth;
+
+    @Value("${email.smtp.starttls.enable}")
+    private String ttlsEnable;
+
+    @Value("${email.debug}")
+    private String debug;
+
+
     @Bean
-    public JavaMailSender getJavaMailSender() {
+    public JavaMailSender getJavaMailSender(){
+
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost(host);
+        mailSender.setPort(Integer.valueOf(port));
 
-        mailSender.setUsername(usernameGmail);
-        mailSender.setPassword(passwordGmail);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.host", "mail.authsmtp.com");
-        props.put("mail.smtp.port", "mail.authsmtp.com");
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        mailSender.setJavaMailProperties(getMailSenderProperties());
 
         return mailSender;
+    }
+
+    private Properties getMailSenderProperties(){
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", ttlsEnable);
+        props.put("mail.debug", debug);
+
+        return props;
     }
 }
--- src/main/java/com/nh7/ecommerce/controller/api/RegisterApi.java
+++ src/main/java/com/nh7/ecommerce/controller/api/RegisterApi.java
@@ -1,5 +1,6 @@
 package com.nh7.ecommerce.controller.api;
 
+import com.nh7.ecommerce.service.EmailService;
 import com.nh7.ecommerce.service.MailService;
 import com.nh7.ecommerce.service.UserService;
 import com.nh7.ecommerce.util.VerifyNumberUtil;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 @RestController
@@ -16,26 +18,36 @@ import java.util.List;
 @ControllerAdvice
 @RequestMapping(value="/api/register")
 public class RegisterApi {
+//    @Autowired
+//    private MailService notificationService;
+//    @Autowired
+//    private UserService userService;
+//    @Autowired
+//    private VerifyNumberUtil verifyNumberUtil;
+//    @GetMapping
+//    public ResponseEntity<Integer> sendEmail(@RequestParam(value="email-address") String emailAddress){
+//        List<Long> idOfMail = userService.findIdsByEmailAddress(emailAddress);
+//        System.out.println("SIZE EMAIL"+idOfMail.size());
+//        if(idOfMail.size() == 0 ){
+//            try {
+//                Integer generatedNum = verifyNumberUtil.generate(5);
+//                notificationService.sendEmail(emailAddress,generatedNum);
+//                return new ResponseEntity<>(generatedNum, HttpStatus.OK);
+//            } catch (MailException mailException) {
+//                System.out.println(mailException);
+//            }
+//        }
+//        return new ResponseEntity<>(-1, HttpStatus.OK);
+//    }
     @Autowired
-    private MailService notificationService;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private VerifyNumberUtil verifyNumberUtil;
-    @GetMapping
-    public ResponseEntity<Integer> sendEmail(@RequestParam(value="email-address") String emailAddress){
-        List<Long> idOfMail = userService.findIdsByEmailAddress(emailAddress);
-        System.out.println("SIZE EMAIL"+idOfMail.size());
-        if(idOfMail.size() == 0 ){
-            try {
-                Integer generatedNum = verifyNumberUtil.generate(5);
-                notificationService.sendEmail(emailAddress,generatedNum);
-                return new ResponseEntity<>(generatedNum, HttpStatus.OK);
-            } catch (MailException mailException) {
-                System.out.println(mailException);
-            }
+    private EmailService emailService;
+    @RequestMapping(value = "" , method = RequestMethod.GET)
+    public void sendEmail(@RequestParam("email-address") String email){
+        try {
+            emailService.sendMail(email, "Test Subject", "TestMessage");
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
-        return new ResponseEntity<>(-1, HttpStatus.OK);
     }
 
 }
